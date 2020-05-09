e5a8a153a7a98499d61ce5014f75301aa1eae986
lyndonteng <lyndon@live.com.sg>
2020-05-09 16:49:25 +0800

    added basic routines for bananagrams

--- database.py
+++ database.py
@@ -1,32 +1,67 @@
 import random
 
-letters = {"a": 3, "b": 3, "c": 3, "d": 3, "e": 3, "f": 3, "g": 3,
-           "h": 3, "i": 3, "j": 3, "k": 3, "l": 3, "m": 3, "n": 3,
-           "o": 3, "p": 3, "q": 3, "r": 3, "s": 3, "t": 3, "u": 3,
-           "v": 3, "w": 3, "x": 3, "y": 3, "z": 3
-           }
+#the pile
+LETTERS = {"a": 13, "b": 3, "c": 3, "d": 6, "e": 18, "f": 3, "g": 4,
+           "h": 3, "i": 12, "j": 2, "k": 2, "l": 5, "m": 3, "n": 8,
+           "o": 11, "p": 3, "q": 2, "r": 9, "s": 6, "t": 9, "u": 6,
+           "v": 3, "w": 3, "x": 2, "y": 3, "z": 2}
 
-no_of_players = sum(players)
+#the game
+class Game:
+    def __init__(self, name):
+        self.name = name
+        self.players = []
+        self.letters = LETTERS
 
+    def add_player(self, player):
+        self.players.append(player)
+
+    def distribute(self):
+        if len(self.players) <= 4:
+            n = 21
+        elif len(self.players) <= 6:
+            n = 15
+        else:
+            n = 11
+
+        while n > 0:
+            n -= 1
+            for player in self.players:
+                player.draw(self.letters)
+
+    def peel(self):
+        # all players draw one card
+        if sum(self.letters.values()) > len(self.players):
+            for player in self.players:
+                player.draw(self.letters)
+
+
+# the player
 class Player:
     def __init__(self, name):
         self.name = name
         self.tiles = {}
 
-    def draw(self):
-        if sum(letters.values()) > no_of_players:
-            letter = random.choice(list(letters.keys()))
-            if letters[letter] > 0:
-                letters[letter] -= 1
+    def draw(self, letters):
+        letter = random.choice(list(letters.keys()))
+        if letters[letter] > 0:
+            letters[letter] -= 1
+            if letter not in self.tiles.keys():
+                self.tiles[letter] = 1
+            else:
                 self.tiles[letter] += 1
 
-def peel():
-    #every player gets one more letter at random
+    def dump(self, letters):
+        # return 1
+        #draw 3
+        n = 3
+        while n > 0:
+            self.draw(letters)
+        pass
 
 
-def dump():
-    #return 1
-    #pick up 3
-
-#check if no of tiles remaining is < no. of players.
-
+if __name__ == '__main__':
+    new_game = Game("new_game")
+    Lyndon = Player("Lyndon")
+    new_game.add_player(Lyndon)
+    new_game.distribute()
