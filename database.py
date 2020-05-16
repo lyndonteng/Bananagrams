4085bea6e33562496089b3ae170c490ccb76be19
lyndonteng <lyndon@live.com.sg>
2020-05-16 18:08:39 +0800

    Added tkinter functionality

--- database.py
+++ database.py
@@ -1,10 +1,16 @@
 import random
+from PIL import Image, ImageTk
 
 #the pile
 LETTERS = {"a": 13, "b": 3, "c": 3, "d": 6, "e": 18, "f": 3, "g": 4,
            "h": 3, "i": 12, "j": 2, "k": 2, "l": 5, "m": 3, "n": 8,
            "o": 11, "p": 3, "q": 2, "r": 9, "s": 6, "t": 9, "u": 6,
            "v": 3, "w": 3, "x": 2, "y": 3, "z": 2}
+
+#images
+# z = Image.open("z.jpg")
+# z.resize((250, 250), Image.ANTIALIAS)
+# z = ImageTk.PhotoImage(z)
 
 #the game
 class Game:
